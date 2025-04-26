e4e410355b865a4de700642a277cf66e7e9b1345
adrian-wen-j-chiang <adrian.wen.j.chiang@accenture.com>
2025-04-26 19:34:54 +0800

    Added environment build variables and scripts
    - Added environment variables for prod, staging, and dev
    - Added scripts to build and serve different configs
    - Disabled Ngxs Logger-Plugin in prod environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,15 +7,17 @@ import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
 import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
 import { withNgxsRouterPlugin } from '@ngxs/router-plugin';
 import { provideStore } from '@ngxs/store';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(), provideStore(
-[],
-withNgxsReduxDevtoolsPlugin(),
-withNgxsLoggerPlugin(),
-withNgxsRouterPlugin())
+      [],
+      withNgxsReduxDevtoolsPlugin(),
+      withNgxsLoggerPlugin({disabled: environment.production}),
+      withNgxsRouterPlugin()
+    )
   ]
 };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://allianz-api-stg.hackydoodle.com/api/'
+};
